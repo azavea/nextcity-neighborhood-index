17ec124f491670f37ab44621d3c2deeb7ba2dcb2
dmcglone <dmcglone@azavea.com>
2017-04-07 10:06:19 -0400

    Add r script

---
+++ nscore_stress_table.R
@@ -0,0 +1,14 @@
+#load datatables library
+library(DT)
+
+#load neighborhood stress index csv
+nscores <- read.csv("final_neighborhood_scores_and_data.csv")
+
+datatable((nscores), class = 'stripe hover', options = list(
+  initComplete = JS(
+    "function(settings, json) {",
+    "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+    "}")
+)) %>%
+  formatCurrency(c('medinc09', 'medinc14', 'MHS2011', 'MHS2015', 'MHSDiff'), digits = 0, '$') %>%
+  formatPercentage(c('crimeChange', 'incChange', 'popChange', 'pov09', 'pov14', 'MHSDifPerc'), 1)
