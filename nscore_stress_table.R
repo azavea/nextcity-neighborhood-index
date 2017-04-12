906687e611b18b247075ac576acda9c39c5035a9
dmcglone <dmcglone@azavea.com>
2017-04-12 16:40:15 -0400

    removed responsiveness

--- nscore_stress_table.R
+++ nscore_stress_table.R
@@ -6,13 +6,12 @@ library(htmlwidgets)
 nscores <- read.csv("final_neighborhood_scores_and_data.csv")
 
 #create data table as R object
-neighborhood_score_table <- datatable((nscores), extensions = 'Responsive', rownames = FALSE, options = list(
+neighborhood_score_table <- datatable((nscores), rownames = FALSE, options = list(
   initComplete = JS(
     "function(settings, json) {",
     "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
     "}")
 )) %>%
-  formatStyle('TotalScore',  color = 'red', backgroundColor = 'orange', fontWeight = 'bold')  %>%
   formatCurrency(c('medinc09', 'medinc14', 'MHS2011', 'MHS2015', 'MHSDiff'), digits = 0, '$') %>%
   formatPercentage(c('crimeChange', 'incChange', 'popChange', 'pov09', 'pov14', 'MHSDifPerc'), 1)
 
