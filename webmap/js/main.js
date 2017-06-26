8914a424b0990533e0082582fcec8986d0383497
eneedham <eneedham@design.upenn.edu>
2017-06-26 16:38:26 -0400

    updating main.js file

--- webmap/js/main.js
+++ webmap/js/main.js
@@ -20,32 +20,41 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
 //Style function to change style based on quantile breaks
 var Total_Style = function(feature) {
   if (feature.properties.TotalScore <= -6){
-      return {fillColor: "#C35B29", fillOpacity: 0.7, color: "#C35B29", weight: 2};  }
+      return {fillColor: "#BB4107", fillOpacity: 0.7, color: "#BB4107", weight: 2};  }
   if (feature.properties.TotalScore <= -3){
       return {fillColor: "#DC7C4E", fillOpacity: 0.7, color: "#DC7C4E", weight: 2};  }
-  if (feature.properties.TotalScore <= -1){
-      return {fillColor: "#FEA77D", fillOpacity: 0.7, color: "#FEA77D", weight: 2};  }
-  if (feature.properties.TotalScore === 0){
-      return {fillColor: "#DFF6F5", fillOpacity: 0.7, color: "#DFF6F5", weight: 2};  }
   if (feature.properties.TotalScore <= 5){
-      return {fillColor: "#7BC0BD", fillOpacity: 0.7, color: "#7BC0BD", weight: 2};  }
+      return {fillColor: "#C1B5C6", fillOpacity: 0.7, color: "#9D88A6", weight: 2};  }
   if (feature.properties.TotalScore <= 8){
-      return {fillColor: "#4C9A97", fillOpacity: 0.7, color: "#4C9A97", weight: 2};  }
+      return {fillColor: "#6F4D8F", fillOpacity: 0.7, color: "#6F4D8F", weight: 2};  }
   if (feature.properties.TotalScore <= 14){
-      return {fillColor: "#1A716E", fillOpacity: 0.7, color: "#1A716E", weight: 2};  }
+      return {fillColor: "#1F0439", fillOpacity: 0.7, color: "#1F0439", weight: 2};  }
 
 };
+
+// var Total_Style = function(feature) {
+//   if (feature.properties.TotalScore <= -6){
+//       return {fillColor: "#C35B29", fillOpacity: 0.7, color: "#C35B29", weight: 2};  }
+//   if (feature.properties.TotalScore <= -3){
+//       return {fillColor: "#DC7C4E", fillOpacity: 0.7, color: "#DC7C4E", weight: 2};  }
+//   if (feature.properties.TotalScore <= 5){
+//       return {fillColor: "#DFF6F5", fillOpacity: 0.7, color: "#7BC0BD", weight: 2};  }
+//   if (feature.properties.TotalScore <= 8){
+//       return {fillColor: "#4C9A97", fillOpacity: 0.7, color: "#4C9A97", weight: 2};  }
+//   if (feature.properties.TotalScore <= 14){
+//       return {fillColor: "#1A716E", fillOpacity: 0.7, color: "#1A716E", weight: 2};  }
+//
+// };
 
 //Changing the opacity and line weight for when a feature is moused over
 var overStyle = {
-  fillOpacity: 1,
-  weight: 4,
+  fillOpacity: 0.3,
+  weight: 6,
 };
-
 
 //Changing the opacity and line weight back to original
 var outStyle = {
-  fillOpacity: 0.8,
+  fillOpacity: 0.7,
   weight: 2
 };
 
@@ -54,7 +63,13 @@ var view = [];
 
 //Function that dictates what happens to each feature of the leaflet layer
 var eachFeature = function(feature, layer) {
-  layer.bindLabel(layer.feature.properties.Neighborho + " has a Total Score of " + "<b>" + layer.feature.properties.TotalScore + "</b>");
+  layer.bindLabel("<div id='text'>" + "<div id='name'>" + layer.feature.properties.Neighborho +
+  "</div>" + " is " + "<b>" + "<span id='category'>" + '<style>#category{background-color:' + layer.feature.properties.color + '; padding: 5px;}</style>' +
+   layer.feature.properties.category + "</b>" + "</span>" + "</div>" +
+   "<br>" + "<table style='width:100%'>" + "<tr>" + "<td>Population Index</td>" + "<td>"+layer.feature.properties.PopScore + "</td>" + "</tr>" +
+   "<tr>" + "<td>Crime Index</td>" + "<td>" + layer.feature.properties.CrimeScore + "</td>" + "</tr>" +
+   "<tr>" + "<td>Median HH Income Index</td>" + "<td>"+layer.feature.properties.MHIScore + "</td>" + "</tr>" +
+   "<tr>" + "<td>Poverty Index</td>" + "<td>"+layer.feature.properties.PovScore + "</td>"+ "</tr>" + "</table>" );
 
   /* =====================
   The following code will run every time a feature on the map is moused over.
@@ -82,7 +97,7 @@ var eachFeature = function(feature, layer) {
 };
 
 //Creating variable for the data
-var dataset = 'https://raw.githubusercontent.com/azavea/nextcity-neighborhood-index/master/phila_neighborhoods_stress_index.geojson';
+var dataset = 'https://raw.githubusercontent.com/azavea/nextcity-neighborhood-index/master/phila_neighborhoods_stress_index_v3.geojson';
 
 //calling the data from github
 $.getJSON(dataset, function(response) {
@@ -95,21 +110,17 @@ $.getJSON(dataset, function(response) {
        });
 
 
-
-
-
-
 //Function to assign the colors to the legend labels
 function getColor(d) {
-    return d == "-12 to -6"? '#C35B29' :
-           d == "-5 to -3" ? '#DC7C4E' :
-           d == "-2 to -1" ? '#FEA77D' :
-           d == "0"        ? '#DFF6F5' :
-           d == "1 to 5"   ? '#7BC0BD' :
-           d == "6 to 8"   ? '#4C9A97' :
-           d == "9 to 14"  ? '#1A716E' :
-                             '#FFEDA0';
+    return d == "Facing Challenges" ? '#BB4107' :
+           d == "Falling Behind"    ? '#DC7C4E' :
+           d == "Keeping Pace"      ? '#C1B5C6' :
+           d == "Making Progress"   ? '#6F4D8F' :
+           d == "Ahead of the Game" ? '#1F0439' :
+                                      '#FFEDA0';
 }
+
+
 
 //We might use this later, if there are different colors for the borders, they will need to be added
 //separately to the legend
@@ -132,7 +143,7 @@ legend.onAdd = function (map) {
 
 //Creating the div for the legend
     var div = L.DomUtil.create('div', 'info legend title'),
-        grades = ["-12 to -6", "-5 to -3", "-2 to -1", "0", "1 to 5", "6 to 8", "9 to 14" ];
+        grades = ["Facing Challenges", "Falling Behind", "Keeping Pace", "Making Progress", "Ahead of the Game" ];
 
 //Legend label
     div.innerHTML += '<b>Score</b><br>';  // don't forget the break tag
